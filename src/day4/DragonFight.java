ab34ac26efa0df68f310cc0fa6f970e118c07578
zhongj3803 <league@WTS7.attlocal.net>
2018-01-04 14:56:20 -0800

    Day 4

--- src/day4/DragonFight.java
+++ src/day4/DragonFight.java
@@ -12,54 +12,62 @@ public class DragonFight {
 		
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
-	
+	int playerHealth = 100;
 		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
-		
+		int dragonHealth = 100;
 		// 4. Create a variable to hold the damage the player's attack does each round
-		
+		int playerDamage = 0;
 		// 5. Create a variable to hold the damage the dragon's attack does each round
-		
+		int dragonDamage = 0;
 		
 		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
+		while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
 		
 		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
 		
 		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell or a kick
-		
+		String  answer = JOptionPane.showInputDialog("Do you wish to attack the dragon with a yell, or a kick?");
 		// 9. If they typed in "yell":
-		
+		if(answer.equals("yell")) {
 			//-- Find a random number between 0 and 10 and store it in dragonDamage
+			dragonDamage = new Random().nextInt(10);
 			
 			//-- Subtract that number from the dragon's health variable 
-			
+		dragonHealth-=dragonDamage;
+		}
 		// 10. If they typed in "kick":
-		
+		if(answer.equals("kick")) {
 			//-- Find a random number between 0 and 25 and store it in dragonDamage
-			
+			dragonDamage = new Random().nextInt(25);
 			//-- Subtract that number from the dragon's health variable
-			
+			dragonHealth-=dragonDamage;
+		}
 			
 		
 		// 11.  Find a random number between 0 and 35 and store it in playerDamage
-		
+		playerDamage = new Random().nextInt(35);
 		// 12. Subtract this number from the player's health
 		
-		
+		playerHealth-=playerDamage;
 		
 		// 13. If the user's health is less than or equal to 0
-		
+		if(playerHealth<=0) {
 			//-- Tell the user that they lost
-			
+		JOptionPane.showMessageDialog(null, "You lose! Better luck next time.");
+		}
 		
 		// 14. Else if the dragon's health is less than or equal to 0
-		
+		if(dragonHealth<=0) {
 			//-- Tell the user that the dragon is dead and they took a ton of gold!
-			
+		JOptionPane.showMessageDialog(null, "You have defeated the dragon! Here is a ton of gold as a reward!" );	
+		}
 	   //  15.  Else
 			
 			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
-			
+			JOptionPane.showMessageDialog(null, "Your health is "+ playerHealth+", and the dragon's health is "+dragonHealth+". You have lost "+ playerDamage + " health, and the dragon has lost "+ dragonDamage + " health");
+		}
 		}
 	}
+
+	
 
--- src/day4/QuizGame.java
+++ src/day4/QuizGame.java
@@ -1,22 +1,48 @@
 package day4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+	int score = 0;	
 		// 2.  Ask the user a question 
-		
+	String answer = JOptionPane.showInputDialog("What's nine plus ten?");	
 		// 3.  Use an if statement to check if their answer is correct
-		
+	    if(answer.equals("19")) {
+	    
 		// 4.  if the user's answer is correct
-		
+	              
 		// -- add one to their score 
-		
+	     score++;   
+	     
+	    }
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
+	   
+	    answer = JOptionPane.showInputDialog("The Law of Conservation of Mass states that matter cannot be created or ______");
+	 if(answer.equals("destroyed")) {
+		 score++;
+	 }
+        answer = JOptionPane.showInputDialog("Identify the slope of this line: y=234x+62");
+         if(answer.equals("234")) {
+        	 score++;
+         } 	 	 
+       answer=JOptionPane.showInputDialog("When the Continental Army was fighting the British, they often used ________ warfare");
+        	 if(answer.equals("guerilla")) {
+        		 score++;
+        	 }  
+        		answer=JOptionPane.showInputDialog("This person is the author of 'A Midsummer Night's Dream'");
+        		 if(answer.equals("William Shakespeare")) {
+        			 score++;	 
+        		 }
+        		 System.out.println(score); 
+	}	 
+        	 
+	}
+	 
 		// 6.  After all the questions have been asked, print the user's score 
 		
-	}
-}
+	
+
